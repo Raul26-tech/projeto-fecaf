e2a6cc4631cc88bd398882c715570f27776cd53b
Raul Santos <rauletec2020@gmail.com>
2022-05-17 15:50:12 -0300

    Criação do form

--- src/scripts/consultaCep.js
+++ src/scripts/consultaCep.js
@@ -1,1 +1,29 @@
-const cep = document.querySelector('#cep')
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado= document.querySelector('#estado');
+
+
+cep.value = '01001000'
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm'
+    document.body.appendChild(script)
+})
+
+function popularForm(response) {
+
+    if('erro' in response) {
+        alert('CEP inválido')
+        return
+    }
+    
+    console.log(response)
+    rua.value = response.logradouro;
+    bairro.value = response.bairro;
+    cidade.value = response.localidade
+    estado.value = response.uf;
+}
